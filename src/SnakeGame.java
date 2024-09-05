9c4599357fea28418505a15a36305295cfb47f0a
Hustini <ejdersalih02@gmail.com>
2024-09-05 17:33:59 +0200

    added snakehead

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -5,13 +5,38 @@ import java.util.Random;
 import javax.swing.*;
 
 public class SnakeGame extends JPanel {
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     int boardWidth;
     int boardHeight;
+    int tileSize = 25;
+
+    Tile snakeHead;
 
     SnakeGame(int boardHeight, int boardWidth) {
         this.boardHeight = boardHeight;
         this.boardWidth = boardWidth;
         setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
         setBackground(Color.BLACK);
+
+        snakeHead = new Tile(5, 5);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.GREEN);
+        g.fillRect(snakeHead.x, snakeHead.y, tileSize, tileSize);
     }
 }
