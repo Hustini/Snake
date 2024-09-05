d0f31a08b85d93014982debdbb0180993b1aee7c
Hustini <ejdersalih02@gmail.com>
2024-09-05 17:37:30 +0200

    changed position off head. now 5 tiles and not pixels

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -37,6 +37,6 @@ public class SnakeGame extends JPanel {
 
     public void draw(Graphics g) {
         g.setColor(Color.GREEN);
-        g.fillRect(snakeHead.x, snakeHead.y, tileSize, tileSize);
+        g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
     }
 }
