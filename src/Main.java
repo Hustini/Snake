0710a982bc2c2a9671352ee0a02bab08d342ce9c
Hustini <ejdersalih02@gmail.com>
2024-09-05 18:21:20 +0200

    added comments to make sure I understand everything

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Frame size
         int boardWidth = 600;
         int boardHeight = 600;
 
+        // Frame
         JFrame frame = new JFrame("Snake");
         frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
@@ -12,6 +14,7 @@ public class Main {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Add snake game to frame
         SnakeGame snakeGame = new SnakeGame(boardHeight, boardWidth);
         frame.add(snakeGame);
         frame.pack();
